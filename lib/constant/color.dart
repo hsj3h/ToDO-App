fa51532c83ff6cc9aa64b0f118690a5ceb2a1f71
hareesh <hareesh7111@mail.com>
2025-05-15 16:32:57 +0530

    initial commit

---
+++ lib/constant/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF4A90E2);   // Sky Blue
+const Color secondaryColor = Color(0xFF7ED6A3); // Mint Green
+const Color accentColor = Color(0xFFFF6B6B);    // Coral Orange
+const Color backgroundColor = Color(0xFFFAFAF0); // Soft Ivory
+const Color textColor = Color(0xFF2E2E2E);      // Charcoal Gray
