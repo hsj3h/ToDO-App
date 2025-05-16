fadea1fd9fcd31309eea2d438e6c21a516fa0b80
hareesh <hareesh7111@mail.com>
2025-05-16 16:35:11 +0530

    change2

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/constant/color.dart';
+import 'package:to_do_app/constant/searchbox.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,29 +8,55 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Icon(Icons.menu,
-          color: primaryColor,
-          size: 30,
-          ),
-          SizedBox(
-            height: 40,
-            width: 30,
-            child: ClipRRect(
-            child:Image.asset("assets/avatar.jpg") ,
-            ),
-          )
-        ],
-      ),
       backgroundColor: backgroundColor,
-      
-       ),
+      appBar: _buildAppBar(),
        body: Container(
-        child: Text('This is home screen'),
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+        child: Column(
+          children: [
+           Searchbox(),
+           ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 50,
+                  bottom: 20
+                ),
+                child: 
+                Text("all to do"),
+                
+              )
+            ],
+           )
+          ],
+        )
+
        ),
     );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(title:
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(Icons.menu,
+        color: primaryColor,
+        size: 30,
+        ),
+        SizedBox(
+          height: 40,
+          width: 30,
+          child: ClipRRect(
+          // child:Image.asset("assets/avatar.jpg") ,
+          borderRadius: BorderRadius.circular(20),
+          ),
+        )
+      ],
+    ),
+    backgroundColor: backgroundColor,
+    elevation: 0,
+    
+     );
   }
 }
