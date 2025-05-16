e61c505683bc3515a1fce2816626b234fc3346c7
hareesh <hareesh7111@mail.com>
2025-05-16 16:58:27 +0530

    change3

--- lib/home.dart
+++ lib/home.dart
@@ -27,9 +27,48 @@ class Home extends StatelessWidget {
       backgroundColor: backgroundColor,
       
        ),
-       body: Container(
-        child: Text('This is home screen'),
+       body:Container
+       (padding: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 15
        ),
+        child: Column(
+          children: [
+            SearchBox(),
+            
+          ],
+        ))
     );
   }
+}
+Widget SearchBox (){
+  return Container(
+        child: Container(
+              padding:EdgeInsets.symmetric(horizontal: 15) ,
+              decoration: BoxDecoration(
+                color:primaryColor,
+                borderRadius: BorderRadius.circular(20),
+                
+
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: textColor,
+                  ),
+                  prefixIconConstraints: BoxConstraints(
+                    maxHeight: 20,
+                    maxWidth:20
+                  ),
+                  border: InputBorder.none,
+                  hintText: 'Search',
+                  hintStyle: TextStyle(
+                    color: textColor
+                  )
+                ),
+              ),
+            )
+       );
 }
