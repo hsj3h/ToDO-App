0bd890de9db5f8825cb18437e15c83d20bff5a78
hareesh <hareesh7111@mail.com>
2025-05-16 12:54:29 +0530

    change 1

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/constant/color.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,7 +8,23 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title:
-      Text('ToDo')
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.menu,
+          color: primaryColor,
+          size: 30,
+          ),
+          SizedBox(
+            height: 40,
+            width: 30,
+            child: ClipRRect(
+            child:Image.asset("assets/avatar.jpg") ,
+            ),
+          )
+        ],
+      ),
+      backgroundColor: backgroundColor,
       
        ),
        body: Container(
