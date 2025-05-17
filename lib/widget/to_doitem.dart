9bf4ec9d05ef62afa15a8363c0a7c7d63fd72d99
hareesh <hareesh7111@mail.com>
2025-05-17 13:39:34 +0530

    change 5

---
+++ lib/widget/to_doitem.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/constant/color.dart';
+import 'package:to_do_app/model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem({super.key, required this.todo});
+  final ToDo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child:
+      ListTile(
+        onTap: (){
+          print('Clicked on checkbox');
+        },
+        shape:RoundedRectangleBorder(
+          borderRadius:BorderRadius.circular(20),
+        
+        ),
+
+        // Text
+        contentPadding: EdgeInsets.symmetric(horizontal: 20,
+        vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          todo.isDone? Icons.check_box : Icons.check_box_outline_blank,
+        color: secondaryColor,),
+        title: Text(todo.todoText!,style: TextStyle(
+          fontSize: 16,
+          color: accentColor,
+          decoration: todo.isDone? TextDecoration.lineThrough : null,
+        ),
+        
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(
+            vertical: 12,
+          ),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: secondaryColor,
+            borderRadius: BorderRadius.circular(5)
+          ),
+          child:IconButton(
+            color: accentColor,
+            iconSize: 18,
+
+          
+            onPressed: (){
+              print('delete');
+            }, icon: Icon(Icons.delete)),
+        ),
+      ),
+    );
+  }
+}
